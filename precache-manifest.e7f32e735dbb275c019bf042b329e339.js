529ae28685e2f6b652cdb681e76b2b1836732da9
Damian Kalek <damian@teamwire.eu>
2020-01-28 23:21:18 +0100

    Updates

---
+++ precache-manifest.e7f32e735dbb275c019bf042b329e339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90b534f1b07abbc8e4982c6ab3da75a",
+    "url": "/turtle/index.html"
+  },
+  {
+    "revision": "cf0b523bc78f7b5eebcf",
+    "url": "/turtle/static/css/main.80f4094b.chunk.css"
+  },
+  {
+    "revision": "4bcce1d2d68b7a70fabd",
+    "url": "/turtle/static/js/2.f455f76d.chunk.js"
+  },
+  {
+    "revision": "a495caf68eede59e88e341a808ee7a2a",
+    "url": "/turtle/static/js/2.f455f76d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf0b523bc78f7b5eebcf",
+    "url": "/turtle/static/js/main.46166601.chunk.js"
+  },
+  {
+    "revision": "606c4ac4d5523ac1358f",
+    "url": "/turtle/static/js/runtime-main.29084562.js"
+  }
+]);
